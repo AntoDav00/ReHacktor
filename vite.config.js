07f95b216149889a0b629b12f026a53da73c2b5e
Antonio D'Aversa <antonio@MacBook-Pro-di-Antonio.local>
2025-01-10 02:50:39 +0100

    Aggiunte nuove funzionalità: reset password, miglioramenti login e README

--- vite.config.js
+++ vite.config.js
@@ -1,52 +1,58 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import imagemin from 'vite-plugin-imagemin'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    imagemin({
-      gifsicle: {
-        optimizationLevel: 7,
-        interlaced: false
-      },
-      optipng: {
-        optimizationLevel: 7
-      },
-      mozjpeg: {
-        quality: 70
-      },
-      pngquant: {
-        quality: [0.7, 0.9],
-        speed: 4
-      },
-      svgo: {
-        plugins: [
-          {
-            name: 'removeViewBox'
-          },
-          {
-            name: 'removeEmptyAttrs',
-            active: false
-          }
-        ]
-      }
-    })
-  ],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return 'vendor'
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    define: {
+      __APP_ENV__: JSON.stringify(env.APP_ENV),
+    },
+    plugins: [
+      react(),
+      imagemin({
+        gifsicle: {
+          optimizationLevel: 7,
+          interlaced: false
+        },
+        optipng: {
+          optimizationLevel: 7
+        },
+        mozjpeg: {
+          quality: 70
+        },
+        pngquant: {
+          quality: [0.7, 0.9],
+          speed: 4
+        },
+        svgo: {
+          plugins: [
+            {
+              name: 'removeViewBox'
+            },
+            {
+              name: 'removeEmptyAttrs',
+              active: false
+            }
+          ]
+        }
+      })
+    ],
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              return 'vendor'
+            }
           }
         }
-      }
+      },
+      chunkSizeWarningLimit: 1000
     },
-    chunkSizeWarningLimit: 1000
-  },
-  optimizeDeps: {
-    include: ['react', 'react-dom', 'react-router-dom']
+    optimizeDeps: {
+      include: ['react', 'react-dom', 'react-router-dom']
+    }
   }
 })
